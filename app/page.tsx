b72f3b618843e148e2e33c123c161146c64b41c6
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-06 23:51:14 +0530

    Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex flex-col h-screen justify-center">
+      <div className="flex justify-center">
+        <div className="flex flex-col">
+          <div className="text-3xl font-bold p-4">Welcome to Home Page</div>
+          <div> Client Page: Interactive client-side rendering in action</div>
+          <div> Server Page: Optimized static content for SEO</div>
+        </div>
+      </div>
+    </div>
+  );
 }
