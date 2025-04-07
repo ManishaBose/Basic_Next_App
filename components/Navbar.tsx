3e1e40079444757d52b570d1ad4a12778f589f4d
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-07 22:31:24 +0530

    Implement link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,26 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="flex justify-evenly p-10">
-      <button className="border-2 border-slate-300 rounded-xl p-2 cursor-pointer">
+      <Link
+        href="/"
+        className="border-2 border-slate-300 rounded-xl p-2 cursor-pointer"
+      >
         Home
-      </button>
-      <button className="border-2 border-slate-300 rounded-xl p-2 cursor-pointer">
+      </Link>
+      <Link
+        href="/static-page"
+        className="border-2 border-slate-300 rounded-xl p-2 cursor-pointer"
+      >
         Server Page
-      </button>
-      <button className="border-2 border-slate-300 rounded-xl p-2 cursor-pointer">
+      </Link>
+      <Link
+        href="/interactive-page"
+        className="border-2 border-slate-300 rounded-xl p-2 cursor-pointer"
+      >
         Client Page
-      </button>
+      </Link>
     </div>
   );
 }
