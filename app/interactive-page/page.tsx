670098590af1d4cdeb45e8649b60f2f3cd44d76f
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-07 22:25:32 +0530

    Implement interactive route

---
+++ app/interactive-page/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
+export default function () {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex flex-col h-screen justify-center">
+      <div className="flex justify-center">
+        <div className="flex flex-col p-20 md:p-50 lg:p-100">
+          <div className="text-3xl font-bold p-4 pl-0">
+            Welcome to Interactive Page
+          </div>
+          <div>
+            This route features a count button that demonstrates the power of
+            client-side interactivity in Next.js. Click the button and see the
+            count go up! This interactive feature is powered by the "use-client"
+            directive in Next.js, which allows this component to be rendered on
+            the client-side.
+          </div>
+          <div
+            className="border rounded-2xl p-2 w-fit cursor-pointer"
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            count is {count}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
