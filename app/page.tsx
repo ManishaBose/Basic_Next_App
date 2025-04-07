9f7bf64d7419464b69d6905e0fb78075bbf598ab
MANISHA BOSE <manishabose.2001@gmail.com>
2025-04-07 22:01:49 +0530

    Implement static route

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <div className="flex flex-col h-screen justify-center">
       <div className="flex justify-center">
         <div className="flex flex-col">
-          <div className="text-3xl font-bold p-4">Welcome to Home Page</div>
+          <div className="text-3xl font-bold p-4 pl-0">
+            Welcome to Home Page
+          </div>
           <div> Client Page: Interactive client-side rendering in action</div>
           <div> Server Page: Optimized static content for SEO</div>
         </div>
---
+++ app/static-page/page.tsx
@@ -0,0 +1,26 @@
+export default function () {
+  return (
+    <div className="flex flex-col h-screen justify-center">
+      <div className="flex justify-center">
+        <div className="flex flex-col p-20 md:p-50 lg:p-100">
+          <div className="text-3xl font-bold p-4 pl-0">
+            Welcome to Static Page
+          </div>
+          <div>
+            This paraphraph right here is rendered statically using Next.js. By
+            generating the content on the server at build time, Next.js ensures
+            that search engines can easily crawl and index this page, boosting
+            its SEO. Plus, serving static content leads to faster load times and
+            a smoother user experience. And if I need interactivity, Next.js
+            allows to use the "use client" directive for specific components.
+          </div>
+          <br />
+          <div>
+            Pretty cool, right? Now navigate to the 'Client Page' to check how
+            interactivity is added is added in Next.js!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
